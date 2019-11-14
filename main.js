80622cde114aff1ec0df261c1319497e9c8d3eba
Fernanda D'Emery <nandademery2@hotmail.com>
2019-11-14 10:21:12 +0100

    styling and tables

--- main.js
+++ main.js
@@ -4,34 +4,45 @@ console.log(data.results[0].members);
 // To format senate data as a table
 
 function myTable(senate) {
+  var tbody = document.getElementById("tbody");
+  tbody.innerHTML = "";
+  for (var i = 0; i < senate.length; i++) {
+    var tr = document.createElement("tr");
 
-    var tbody = document.getElementById("tbody");
-    tbody.innerHTML = "";
-    for (var i = 0; i < senate.length; i++) {
+    var name = document.createElement("td");
 
+    var senatorName = senate[i].last_name +
+      " " +
+      senate[i].first_name +
+      " " +
+      (senate[i].middle_name || "");
 
-        var tr = document.createElement("tr");
+    if (!senate[i].url) {
+      name.textContent = senatorName
+    } else {
+      var nameLink = document.createElement("a");
+      nameLink.textContent = senatorName;
+      nameLink.setAttribute("href", senate[i].url);
+      nameLink.setAttribute("target", "_blank");
 
-        var name = document.createElement("td");
-        name.textContent = senate[i].last_name + " " + senate[i].first_name + " " + (senate[i].middle_name || "");
-
-        var party = document.createElement("td");
-        party.textContent = senate[i].party;
-
-        var state = document.createElement("td");
-        state.textContent = senate[i].state;
-
-        var seniority = document.createElement("td");
-        seniority.textContent = senate[i].seniority;
-
-        var percVotes = document.createElement("td");
-        percVotes.textContent = senate[i].votes_with_party_pct;
-
-        tr.append(name, party, state, seniority, percVotes); // these functions push the data into the HTML.
-        tbody.appendChild(tr);
-
+      name.appendChild(nameLink);
     }
 
+    var party = document.createElement("td");
+    party.textContent = senate[i].party;
+
+    var state = document.createElement("td");
+    state.textContent = senate[i].state;
+
+    var seniority = document.createElement("td");
+    seniority.textContent = senate[i].seniority;
+
+    var percVotes = document.createElement("td");
+    percVotes.textContent = senate[i].votes_with_party_pct;
+
+    tr.append(name, party, state, seniority, percVotes); // these functions push the data into the HTML.
+    tbody.appendChild(tr);
+  }
 }
 
 myTable(members);
