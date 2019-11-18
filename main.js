e75b390ad30520734227729a64f1b2e44de40a22
Fernanda D'Emery <nandademery2@hotmail.com>
2019-11-18 17:27:36 +0100

    Added filters to House and Senate data pages

--- main.js
+++ main.js
@@ -46,3 +46,97 @@ function myTable(senate) {
 }
 
 myTable(members);
+
+// function to filter table by party
+
+document.getElementById("republican").addEventListener("change", selectParty);
+document.getElementById("democrat").addEventListener("change", selectParty);
+document.getElementById("independent").addEventListener("change", selectParty);
+
+function selectParty() {
+  var republican = document.getElementById("republican");
+  var democrat = document.getElementById("democrat");
+  var independent = document.getElementById("independent");
+
+  var selected = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (republican.checked && members[i].party == "R") {
+      selected.push(members[i]);
+    }
+    if (democrat.checked && members[i].party == "D") {
+      selected.push(members[i]);
+    }
+    if (independent.checked && members[i].party == "I") {
+      selected.push(members[i]);
+    } else if (
+      republican.checked == false &&
+      democrat.checked == false &&
+      independent.checked == false
+    ) {
+      myTable(members);
+    } else {
+      myTable(selected);
+    }
+  }
+}
+
+// To create list with states in html.
+
+var listStates = [];
+
+
+function createListStates(membersArray) {
+
+  for (var i = 0; i < membersArray.length; i++) {
+    if (!listStates.includes(membersArray[i].state)) {
+
+      listStates.push(membersArray[i].state);
+
+    }
+  }
+  listStates.sort();
+  for (var i = 0; i < listStates.length; i++) {
+    var option = document.createElement("option");
+    option.textContent = listStates[i];
+    option.setAttribute("value", listStates[i]);
+    document.getElementById("state-filter").append(option);
+  }
+}
+createListStates(members);
+
+
+// Filter by State
+document.getElementById("state-filter").addEventListener("change", filterState);
+
+
+function filterState(membersArr) {
+  var selectedStates = [];
+  membersArr = members;
+
+  var state = document.getElementById("state-filter").value;
+  console.log(state);
+
+  if (state == "all") {
+    myTable(members);
+  } else {
+
+    for (var i = 0; i < membersArr.length; i++) {
+
+      if (state == membersArr[i].state) {
+        selectedStates.push(membersArr[i]);
+
+      }
+
+
+
+
+    }
+
+    console.log(selectedStates);
+    myTable(selectedStates)
+  }
+}
+
+
+filterState(members);
